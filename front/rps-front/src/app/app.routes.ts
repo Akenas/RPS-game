bfd94c05c4b3f9a0e520a213ab44e6f6e3c5e6ff
Kevin Sender <ksender93@gmail.com>
2024-11-07 19:33:11 +0100

    basic-front-end
    - feat: added basic guard to protect pages.
    As we will be fetching data from the backend on every other page but login/signup we will track 403 status to further protect the pages just in case

--- front/rps-front/src/app/app.routes.ts
+++ front/rps-front/src/app/app.routes.ts
@@ -2,15 +2,18 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { HomeComponent } from './ui/home/home.component';
+import { AuthGuard } from './auth.guard';
 
 export const routes: Routes = [
     {
         path: '',
-        component: HomeComponent
+        component: HomeComponent,
+        canActivate: [AuthGuard]
     },
     {
         path: 'login',
         component: LoginComponent
+        
     },
     {
         path: 'signup',
