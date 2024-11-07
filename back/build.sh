c63790c31e57191525cb211e83315b726f9e14ba
Kevin Sender <ksender93@gmail.com>
2024-11-07 19:10:43 +0100

    docker-compose
    - feat: created Dockerfile for backend service. This will run mvn package and create the docker image.
    - feat: crated docker-compose.yml to wire mysqldb and the backend service for now
    - fix: changed properties from backend servie to use the correct mysqldb port even we set it up on the docker-compose file
    - feat: deleted build.sh

--- back/build.sh
+++
@@ -1,6 +0,0 @@
-# Extract project name and version
-PROJECT_NAME=$(mvn help:evaluate -Dexpression=project.name -q -DforceStdout)
-PROJECT_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
-
-# Build the Docker image with extracted name and version
-docker build -t "${PROJECT_NAME}:${PROJECT_VERSION}" .
