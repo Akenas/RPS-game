d8f036d904730ccebb6f4f31bf33ab5bf9686749
Kevin Sender <ksender93@gmail.com>
2024-11-08 17:51:13 +0100

    basic-front-end
    - feat: modified basic-page.component to add logo, wrap the content and dinamically set the page id

--- front/rps-front/src/app/ui/pages/basic-page/basic-page.component.ts
+++ front/rps-front/src/app/ui/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
@@ -9,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class BasicPageComponent {
 
+  @Input() pageId: string = 'default-id';
 }
