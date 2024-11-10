7a81ca319309754a6c4f7d84f47cfa73589463c0
Kevin Sender <ksender93@gmail.com>
2024-11-10 19:38:23 +0100

    basic-front-end
    - feat: added more model classes

---
+++ front/rps-front/src/app/model/gameMatch.model.ts
@@ -0,0 +1,13 @@
+import { ModeOption } from "./mode-option.model";
+import { Player } from "./player.model";
+import { Round } from "./roud.model";
+
+export interface GameMatch {
+    id: number;
+    player1: Player;
+    player2: Player;
+    mode: ModeOption;
+    winner: number;
+    rounds: Round[];
+    ongoing:boolean;
+  }
