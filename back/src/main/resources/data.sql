6af71c993f3ba2bcd6edfa70b5a3ce1c2538f33b
Akenas <ksender93@gmail.com>
2024-11-13 19:12:13 +0100

    fast-hotfixes-before-release
    fix: online play works on the backend but it needs more work on the front. Disabling the data insertion on init.
    fix: added button on mode selection to go back to home page
    - feat: changed favicon and page title
    - feat: image compression for faster loading

--- back/src/main/resources/data.sql
+++ back/src/main/resources/data.sql
@@ -12,6 +12,7 @@ WHERE NOT EXISTS ( SELECT NULL
 INSERT INTO game_modes
 SELECT *
 FROM ( SELECT 1 as ID,'BO5 vs IA',1 as mode,1 UNION ALL
-       SELECT 2 as ID,'BO5 vs Player',0 as mode,1 ) data
+       --SELECT 2 as ID,'BO5 vs Player',0 as mode,1 
+       ) data
 WHERE NOT EXISTS ( SELECT NULL
                    FROM game_modes AS t2 );
